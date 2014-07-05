427803e0f466b5c47e84477f2cf59e6cc56d3b6f
jcress <jcress@crunchbang.(none)>
2014-07-05 15:41:55 -0400

    changes during system deploy

--- install_vim.sh
+++ install_vim.sh
@@ -16,15 +16,15 @@ sudo apt-get install gtk2-engines-pixbuf
 
 
 # Setup Pathogen to manage your plugins
-mkdir -p ~/.dotfiles/vim/autoload ~/.dotfiles/vim/bundle
-cd ~/.dotfiles/vim/autoload
+mkdir -p ~/dotfiles/vim/autoload ~/dotfiles/vim/bundle
+cd ~/dotfiles/vim/autoload
 wget -O pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 
 #Color scheme
-mkdir -p ~/.dotfiles/vim/colors && cd ~/.dotfiles/vim/colors
+mkdir -p ~/dotfiles/vim/colors && cd ~/dotfiles/vim/colors
 wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
 
-cd ~/.dotfiles/vim/bundle
+cd ~/dotfiles/vim/bundle
 # Settings for vim-powerline
 git clone git://github.com/Lokaltog/vim-powerline.git
 # Settings for ctrlp
